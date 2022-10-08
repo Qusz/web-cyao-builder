8edaed6919ef9f0300812eeae1c4c8605677582e
Denis Gorchakov <32505327+Qusz@users.noreply.github.com>
2022-10-08 08:30:53 +0800

    Now text for Default state is stored in data.js intead

--- src/js/data.js
+++ src/js/data.js
@@ -46,3 +46,5 @@ export const data = [
     option_id: ["o_21", "o_22", "o_23", "o_24"]
   }
 ]
+
+export const welcome = 'Game preface. Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo.';
--- src/js/states.js
+++ src/js/states.js
@@ -1,4 +1,4 @@
-import { data } from './data.js';
+import { data, welcome } from './data.js';
 import { showButton, renderOptions } from './utilities.js';
 
 const optionsBox = document.querySelector('.main-box-options'),
@@ -17,7 +17,7 @@ export class PageState {
 
 export class DefaultState {
   constructor() {
-    textBox.textContent = 'Game preface. Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo.';
+    textBox.textContent = welcome;
     showButton('btn btn-new-game', 'New Game', '4rem auto 0 auto', textBox);
   }
 };
